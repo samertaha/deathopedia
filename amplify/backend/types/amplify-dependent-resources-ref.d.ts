bd39c656c540b56aa4de054ab04d230fa759c263
Samer taha <alrazy1@gmail.com>
2022-11-21 17:28:58 +0200

    prepaired appsync locally and created posts collection

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "deathopediaapp": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
